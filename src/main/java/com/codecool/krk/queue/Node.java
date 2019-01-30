c4e08eaf526a17cfdc50bd4eb47935f7fe912236
Paprok <rafal.becla@gmail.com>
2019-01-30 21:15:04 +0100

    passed enqueue tests

--- src/main/java/com/codecool/krk/queue/Node.java
+++ src/main/java/com/codecool/krk/queue/Node.java
@@ -1,4 +1,37 @@
 package com.codecool.krk.queue;
 
 public class Node {
+    private Node next;
+    private String content;
+    private int priority;
+
+    public Node(String content) {
+        this.content = content;
+        this.priority = 0;
+    }
+
+    public Node(String content, int priority) {
+        this.content = content;
+        this.priority = priority;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
--- src/main/java/com/codecool/krk/queue/Queue.java
+++ src/main/java/com/codecool/krk/queue/Queue.java
@@ -1,4 +1,48 @@
 package com.codecool.krk.queue;
 
 public class Queue {
+    private Node first;
+    private Node last;
+    private int size;
+
+    public Queue() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return this.first == null;
+    }
+
+    public void enqueue(String value) {
+        Node newNode = new Node(value);
+        if (this.last == null) {
+            this.first = newNode;
+        } else {
+            this.last.setNext(newNode);
+        }
+        this.last = newNode;
+        this.size++;
+    }
+
+    public String peek(){
+        return this.last.getContent();
+    }
+
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        Node node = this.first;
+        while (node != null) {
+            builder.append(node.getContent());
+            builder.append(' ');
+            node = node.getNext();
+        }
+        return builder.toString().trim();
+    }
 }
